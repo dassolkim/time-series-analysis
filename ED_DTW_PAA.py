41d33f597e7373eedfa5e7777956487a2bf098ee
HILTON731 <hilton3180@gmail.com>
2020-04-09 23:54:55 +0900

    fixed

--- ED_DTW_PAA.py
+++ ED_DTW_PAA.py
@@ -6,6 +6,7 @@ from scipy.spatial.distance import euclidean
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+
 def PAA(data, segment):
     
     len_data = len(data)
@@ -18,9 +19,9 @@ def PAA(data, segment):
         res = np.array([item.mean() for item in sectionedArr])
 
     else:
-        value_space= np.arange(0, len_data * segment)#[0,...,길이*분할 수]
-        output_index = value_space // len_data#[0,...,길이*분할 수]/길이
-        input_index = value_space // segment#[0,...,길이*분할 수]/분할 수
+        value_space= np.arange(0, len_data * segment)   #[0,...,길이*분할 수]
+        output_index = value_space // len_data          #[0,...,길이*분할 수]/길이
+        input_index = value_space // segment            #[0,...,길이*분할 수]/분할 수
         uniques, nUniques = np.unique(output_index, return_counts=True)#배열의 고유한 요소를 찾는다. uniques: 고유한 값을 제공하는 입력 배열의 인덱스, nUniques: 입력 배열에 각 고유값이 나타나는 횟수
         # print(uniques,"\n",nUniques)
         # print(input_index)
@@ -43,17 +44,22 @@ def PAA(data, segment):
 data_path = "bitcoin_jihun/DTW/dataset/"
 file_list = os.listdir(data_path)
 file_list_csv = [file for file in file_list if file.endswith(".csv")]
+
 # print(file_list_csv)
 arraysA = pd.read_csv("bitcoin_jihun/DTW/dataset/{}".format(file_list_csv[10]))
 arraysB = pd.read_csv("bitcoin_jihun/DTW/dataset/{}".format(file_list_csv[7]))
+
 arraysA['Date'] = arraysA['Date'].map(pd.to_datetime)
 arraysB['Date'] = arraysB['Date'].map(pd.to_datetime)
+
 arraysA = np.array(arraysA['Close']).reshape(-1)
 arraysB = np.array(arraysB['Close']).reshape(-1)
+
 distance, path = fastdtw(arraysA, arraysB, dist=euclidean)
 
 arraysA_ = np.array(PAA(arraysA, 100)).reshape(-1)
 arraysB_ = np.array(PAA(arraysB, 300)).reshape(-1)
+
 distance_, path_ = fastdtw(arraysA_, arraysB_, dist=euclidean)
 # distance = np.float(distance)
 print("distance: ",distance)
@@ -63,8 +69,8 @@ fig, ax = plt.subplots(2,1,figsize=(10, 10))
 
 ax[0].plot(arraysA,color='r',label='arraysA',alpha=0.75)
 ax[1].plot(arraysA_,color='r',label='arraysA_',alpha=0.75)
-ax[0].plot(arraysB,color='r',label='arraysB',alpha=0.75)
-ax[1].plot(arraysB_,color='r',label='arraysB_',alpha=0.75)
+ax[0].plot(arraysB,color='b',label='arraysB',alpha=0.75)
+ax[1].plot(arraysB_,color='b',label='arraysB_',alpha=0.75)
 
 
 # arraysA = pd.DataFrame(data=arraysA)
