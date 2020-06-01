9191e215e1572ab700a787c0c26cbbeee16c6c9c
HILTON731 <hilton3180@gmail.com>
2020-06-01 17:16:25 +0900

    svm visualize

--- bitcoin_jihun/SVM/light_svm_example.py
+++ bitcoin_jihun/SVM/light_svm_example.py
@@ -1,18 +1,82 @@
 import numpy as np
-from sklearn import datasets
-from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import LinearSVC
+from sklearn.datasets import load_iris
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
+from sklearn.svm import SVC
+
+def make_meshgrid(x, y, h=0.2):
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy =np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    
+    return xx, yy
+def plot_contours(ax, clf, xx, yy, **params):
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
 
 iris = datasets.load_iris()
-X = iris["data"][:, (2, 3)]
-y = (iris["target"] == 2).astype(np.float64)
 
-svm_clf = Pipeline([
-    ("scaler", StandardScaler()),
-    ("linear_svc", LinearSVC(C=1, loss="hinge")),
-])
+X = iris.data
+y = iris.target
 
-svm_clf.fit(X, y)
+C_range =  np.logspace(-2, 10, 13)
+gamma_range = np.logspace(-9, 3, 13)
+param_grid = dict( gamma=gamma_range, C=C_range)
 
-print(svm_clf.predict([[5.5, 1.7]]))
+cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv)
+grid.fit(X, y)
+print("The best parameters are %s with a score of %0.2f"
+      % (grid.best_params_, grid.best_score_))
+
+C = []
+gamma = []
+
+print(param_grid)
+
+X = X[:, :2]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=42)
+
+for this_C in  np.logspace(-2, 10, 13):
+    clf = SVC(kernel='linear',C=this_C).fit(X_train,y_train)
+    scoretrain = clf.score(X_train, y_train)
+    scoretest = clf.score(X_test, y_test)
+    C.append(scoretest)
+    print("Linear SVM value of C:{}, training score: {:2f}, Test Score: {:2f}\n".format(this_C,scoretrain,scoretest))
+
+C = np.logspace(-2, 7, 10)[C.index(max(C))]
+
+models = (svm.SVC(kernel='linear', C=C),
+         svm.LinearSVC(C=C, max_iter=10000),
+         svm.SVC(kernel='rbf', gamma=0.7, C=C),
+         svm.SVC(kernel='poly',degree=3,gamma='auto',C=C))
+models = (clf.fit(X, y) for clf in models)
+
+titles = ('SVC with linear kernel',
+          'LinearSVC (linear kernel)',
+          'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel')
+
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X[:, 0], X[:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
+
+plt.show()
